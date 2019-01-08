bc35a4ff9dfa7880bca38595ae44524b6ae564f3
Agris Markus <agris@Agriss-MacBook-Pro.local>
2019-01-08 02:17:56 +0200

    fix

--- src/functions.php
+++ src/functions.php
@@ -7,7 +7,7 @@ function _c($key, $options = [], $languageCode = null)
     $transService = new \Paylatergroup\Translation\Services\ContentTranslationService();
     $transService->setTranslationKey($key)->setLanguageCode($languageCode);
 
-    Translation::registerService($transService)->translate();
+    \Paylatergroup\Translation\Translation::registerService($transService)->translate();
 }
 
 //function _i($key, $options = [], $languageCode = null)
